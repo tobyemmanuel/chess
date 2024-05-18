1331d2520fe0653d3fc2a23e5b5ccb627775fd89
Oluwatobi Adelabu <adelabutobi@gmail.com>
2024-05-18 02:43:09 +0100

    UI improvement

--- index.php
+++ index.php
@@ -11,11 +11,16 @@
 </head>
 
 <body>
+    <div class="loading-screen" id="loading-screen">
+        <div class="loading-spinner"></div>
+        <div class="loading-text">Please wait</div>
+    </div>
+
     <div id="splashScreen" class="screen">
         <!-- Splash screen content -->
         <h1>3DChess Pro</h1>
         <small>by Tobi Adelabu</small>
-        <button class="primary" id="startButton">Start</button>
+        <button class="primary" id="startButton">Start Playing</button>
     </div>
 
     <div id="mainMenu" class="screen" style="display: none;">
@@ -33,7 +38,7 @@
 
     <div id="loggedMenu" class="screen" style="display: none;">
         <!-- Logged in menu content -->
-        <h2>Welcome Back</h2>
+        <h2 class="welcome-user">Welcome Back</h2>
         <ul>
             <li><a href="#" id="continueGame">Continue Game</a></li>
             <li><a href="#" id="newGame">New Game</a></li>
@@ -41,7 +46,7 @@
             <li><a href="#" class="history">History</a></li>
             <li><a href="#" class="highScores">High Scores</a></li>
             <li><a href="#" class="back">Back</a></li>
-            <li><a href="#" class="exitGame">Exit Game</a></li>
+            <li><a href="#" class="exit">Exit Game</a></li>
         </ul>
     </div>
 
@@ -49,28 +54,29 @@
         <!-- Logged in menu content -->
         <h2>Create Player</h2>
         <form id="createPlayerForm">
-            <label for="playerName">Player Name</label>
-            <input type="text" id="playerName" name="playerName" required>
-            <label for="passCode">Pass Code</label>
-            <input type="text" id="passCode" name="passCode" required>
+            <label for="username">Player Name</label>
+            <input type="text" id="username" name="username" required>
+            <label for="passcode">Pass Code</label>
+            <input type="password" id="passcode" name="passcode" required>
         </form>
-        <button type="submit" class="primary">Create</button>
+        <button type="submit" id="createPlayerAction" class="primary">Create</button>
         <button class="secondary back">Back</button>
     </div>
 
-    
+
     <div id="continuePlayerScreen" class="screen" style="display: none;">
         <!-- Logged in menu content -->
         <h2>Enter Account</h2>
         <form id="continuePlayerForm">
-            <label for="playerName">Player Name</label>
-            <input type="text" id="playerName" name="playerName" required>
-            <label for="passCode">Pass Code</label>
-            <input type="text" id="passCode" name="passCode" required>
+            <label for="username">Player Name</label>
+            <input type="text" id="username" name="username" required>
+            <label for="passcode">Pass Code</label>
+            <input type="password" id="passcode" name="passcode" required>
         </form>
-        <button type="submit" class="primary">Continue</button>
+        <button type="submit"  id="continuePlayerAction" class="primary">Continue</button>
         <button class="secondary back">Back</button>
     </div>
+    
 
     <div id="highScoresScreen" class="screen" style="display: none;">
         <!-- Highscores content -->
@@ -80,7 +86,8 @@
             <li>Player 2 - 400</li>
             <li>Player 3 - 300</li>
         </ul>
-        <button class="secondary exit" id="backToMainMenu">Back</button>
+        <button class="logged-out secondary back" id="backToMainMenu">Back</button>
+        <button class="logged-in secondary back_logged" id="backToMainMenu">Back</button>
     </div>
 
     <div id="historyScreen" class="screen" style="display: none;">
@@ -90,7 +97,8 @@
             <li>Game 1 - Player 1 vs Player 2 - Winner: Player 1</li>
             <li>Game 2 - Player 3 vs Player 4 - Winner: Player 4</li>
         </ul>
-        <button class="secondary back_logged" id="backToMainMenu">Back</button>
+        <button class="logged-out secondary back" id="backToMainMenu">Back</button>
+        <button class="logged-in secondary back_logged" id="backToMainMenu">Back</button>
     </div>
 
     <div id="gameSettingsScreen" class="screen" style="display: none;">
@@ -113,7 +121,7 @@
                 <option value="set1">Regular</option>
             </select>
             <br>
-            <label for="difficulty">Difficulty:</label>
+            <label for="difficulty">Opponent Difficulty:</label>
             <select id="difficulty" name="difficulty">
                 <option value="easy">Easy</option>
                 <option value="medium">Medium</option>
@@ -122,11 +130,13 @@
             <br>
         </form>
         <button class="primary">Save</button>
-        <button class="secondary back" id="backToMainMenu">Back</button>
+        <button class="logged-in secondary back_logged" id="backToLoggedMenu">Back</button>
+        <button class="logged-out secondary back" id="backToMainMenu">Back</button>
     </div>
 
 
     <script src="./assets/js/main.js"></script>
+    <script src="./assets/js/utils.js"></script>
 </body>
 
 </html>
