7aca37dbc7fcac380eae916339e1e845f111ed52
Oluwatobi Adelabu <adelabutobi@gmail.com>
2024-05-20 02:10:57 +0100

    implemented settings validation

--- classes/validations.php
+++ classes/validations.php
@@ -29,4 +29,38 @@ class validations
 
         return [true, "No errors"];
     }
+
+    public static function settings($camera_angle = null, $brightness = null, $difficulty = null, $chessPieceType = null, $boardColor = null)
+    {
+        // Validation logic here
+        if (!is_numeric($camera_angle) || $camera_angle < 0 || $camera_angle > 90) {
+            return [false, "Invalid camera angle"];
+        }
+
+        if (!is_numeric($brightness) || $brightness < 40 || $brightness > 90) {
+            return [false, "Invalid camera angle"];
+        }
+
+        if (!in_array($difficulty, array('easy', 'medium', 'hard'))) {
+            return [false, "Invalid difficulty set"];
+        }
+
+        if (!in_array($chessPieceType, array('set1'))) {
+            return [false, "Invalid chess piece type set"];
+        }
+
+        if (!in_array($boardColor, array('set1'))) {
+            return [false, "Invalid board color set"];
+        }
+
+        return [true, "No errors"];
+    }
+
+    public static function checkLogin(){
+        if(isset($_SESSION['user_id'])) {
+            return [true, $_SESSION['user_id']];
+        };
+        return [false];
+    }
+
 }
